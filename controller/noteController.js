f6c8b5af4297b618c0ba285db2597553964aebc6
zarkonj <zarko_dragojevic_7@hotmail.com>
2017-11-04 14:51:48 +0100

    init

--- controller/noteController.js
+++ controller/noteController.js
@@ -1,1 +1,5 @@
 var store = require("../services/noteStore");
+
+module.exports.shipiChopf = function (req, res) {
+    res.render("note_detail.hbs", { title: "Shipichopf"} );
+}
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var noteController = require('../controller/noteController');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,4 +7,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/create", noteController.shipiChopf)
 module.exports = router;
