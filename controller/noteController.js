388d2e6a0caa3b1c383a16db8cf04c0cf2c8b34d
codebrudi <jan.forlin@hotmail.com>
2017-11-23 10:49:12 +0100

    sort hideNotes works..

--- controller/noteController.js
+++ controller/noteController.js
@@ -4,16 +4,13 @@ let moment = require('moment');
 moment.locale("de");
 
 let reverseSort = false;
-//let sortOrderValue = 1;
-
 
 module.exports.showNotes = function (req, res) {
     store.all(function (err, notes) {
         notes = sortData(req.session.order, notes);
         notes = hideNotes(req.session.hideNote, notes);
         notes = setDoneUntil(notes);
-
-        res.render("showNotes", {title: "Note Pro", changeStyle : req.session.changeStyle, note: notes})
+        res.render("showNotes", {title: "Note Pro",hideNote : req.session.hideNote, changeStyle : req.session.changeStyle, note: notes})
     })
 };
 
@@ -25,61 +22,6 @@ module.exports.saveNote = function (req, res) {
     store.add(req.body.title, req.body.descr, req.body.importance, req.body.doneuntil, req.body.complete, function (err, newDoc) {
         res.redirect('/');
     });
-};
-
-module.exports.sortNote = function (req, res) {
-    res.redirect('/')
-
-
-    /*    switch (req.query.sortBy) {
-            case "importance":
-                if (req.query.sortOrder) {
-                    sortOrderValue = 1;
-                } else {
-                    sortOrderValue = -1;
-                }
-                store.sortImp(sortOrderValue, function (err, data) {
-                        if (req.query.hideNotes) {
-                            data = data.filter(function (obj) {
-                                return (!obj.complete);
-                            })
-                        }
-
-                        res.render("showNotes.hbs", {note: data, query: req.query, changeStyle: getChangeStyle(req)});
-                    }
-                );
-                break;
-            case "doneuntil":
-                if (req.query.sortOrder) {
-                    sortOrderValue = 1;
-                } else {
-                    sortOrderValue = -1;
-                }
-                store.sortDat(sortOrderValue, function (err, data) {
-                        if (req.query.hideNotes) {
-                            data = data.filter(function (obj) {
-                                return (!obj.complete);
-                            })
-                        }
-
-                        res.render("showNotes.hbs", {note: data, query: req.query, changeStyle: getChangeStyle(req)});
-                    }
-                );
-                break;
-            default:
-                store.all(
-                    function (err, data) {
-                        if (req.query.hideNotes) {
-                            data = data.filter(function (obj) {
-                                return (!obj.complete);
-                            })
-                        }
-
-                        res.render("showNotes.hbs", {note: data, query: req.query, changeStyle: getChangeStyle(req)});
-                    }
-                );
-                break;
-        }*/
 };
 
 module.exports.editNote = function (req, res) {
@@ -104,12 +46,10 @@ module.exports.deleteNote = function (req, res) {
 module.exports.invertHideNote = function (req, res) {
     req.session.hideNote = !req.session.hideNote;
     res.redirect('/');
-
-
 };
 
 module.exports.invertChangeStyle = function (req, res) {
-    if(req.session.changeStyle === undefined) {req.session.changeStyle = false;}
+    //if(req.session.changeStyle === undefined) {req.session.changeStyle = false;}
     req.session.changeStyle = !req.session.changeStyle;
     res.redirect('/');
 };
@@ -155,6 +95,11 @@ function sortOrder(val1, val2) {
 
 
 function hideNotes(hideNote, notes) {
+    if(hideNote){
+        notes = notes.filter(function(note){
+            return !note.complete;
+        })
+    }
     return notes;
 }
 
--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,11 @@ router.get('/', noteController.sortNote, function (req, res, next) {
 router.get("/", noteController.showNotes);
 
 router.get("/create", noteController.createNewNote);
-router.get("/showNotes", noteController.sortNote);
 router.get("/editNote", noteController.editNote);
 router.get("/deleteNote", noteController.deleteNote);
 router.get("/hideNotes", noteController.invertHideNote);
 router.get("/changeStyle", noteController.invertChangeStyle);
 router.get("/sort/:order/",noteController.sortNotes);
-
 
 router.post("/create", noteController.saveNote);
 router.post("/editNote", noteController.updateNote);
